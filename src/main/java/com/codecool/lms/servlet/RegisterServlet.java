4c4ba7ddf9596ee05665d0d1c4286dcd6fb083c4
HajnalFeri <kalacsf1@gmail.com>
2018-03-19 15:46:43 +0100

    created register servlet prototype

---
+++ src/main/java/com/codecool/lms/servlet/RegisterServlet.java
@@ -0,0 +1,28 @@
+package com.codecool.lms.servlet;
+
+import com.codecool.lms.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = new User(name, email, password);
+
+        req.setAttribute("register", user);
+
+        req.getRequestDispatcher("something.jsp").forward(req, resp);
+    }
+}
