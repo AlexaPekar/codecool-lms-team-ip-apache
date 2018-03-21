649f70baa5232031c595b3cb372f55d2a7152fd2
Farago Bence <semmiertelme13@gmail.com>
2018-03-21 08:56:22 +0100

    Curriculum servlet created

--- src/main/java/com/codecool/lms/servlet/CurriculumServlet.java
+++ src/main/java/com/codecool/lms/servlet/CurriculumServlet.java
@@ -1,7 +1,9 @@
 package com.codecool.lms.servlet;
 
 import com.codecool.lms.model.Page;
+import com.codecool.lms.model.User;
 import com.codecool.lms.service.PageServiceImpl;
+import com.codecool.lms.service.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,6 +21,8 @@ public class CurriculumServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Page> pages = PageServiceImpl.getPageService().getPages();
+        User currentUser = UserServiceImpl.getUserService().getCurrentUser();
+        req.setAttribute("current", currentUser);
         req.setAttribute("pages", pages);
         req.getRequestDispatcher("home.jsp").forward(req, resp);
     }
