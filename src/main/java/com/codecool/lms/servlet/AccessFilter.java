2718ae188f891982ab1f879e32274a3754a16993
AlexaPekar <missdreambelle@gmail.com>
2018-03-21 16:12:42 +0100

    Create AccessFilter class for authenticational purposes

---
+++ src/main/java/com/codecool/lms/servlet/AccessFilter.java
@@ -0,0 +1,42 @@
+package com.codecool.lms.servlet;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebFilter("/AccessFilter")
+public class AccessFilter implements Filter {
+
+    private ServletContext context;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.context = filterConfig.getServletContext();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+
+
+        if (!session.getAttributeNames().hasMoreElements()) {
+            req.setAttribute("message", "You have to log in first!");
+            req.getRequestDispatcher("index.jsp").forward(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
