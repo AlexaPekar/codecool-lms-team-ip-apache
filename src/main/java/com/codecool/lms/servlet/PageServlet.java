9a75c43124a3b87d26fc9dc1142442c610705d6f
AlexaPekar <missdreambelle@gmail.com>
2018-03-22 13:34:44 +0100

    Examine if user is already submitted in PageServlet class

--- src/main/java/com/codecool/lms/servlet/PageServlet.java
+++ src/main/java/com/codecool/lms/servlet/PageServlet.java
@@ -1,9 +1,11 @@
 package com.codecool.lms.servlet;
 
-import com.codecool.lms.exception.PageNotFoundException;
+import com.codecool.lms.model.Assignment;
+import com.codecool.lms.model.AssignmentPage;
 import com.codecool.lms.model.Page;
 import com.codecool.lms.model.TextPage;
 import com.codecool.lms.service.PageServiceImpl;
+import com.codecool.lms.service.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/showpage")
 public class PageServlet extends HttpServlet {
@@ -18,18 +21,23 @@ public class PageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String title = req.getParameter("title");
-        try {
+
             Page myPage = PageServiceImpl.getPageService().findPageByTitle(title);
             req.setAttribute("page", myPage);
+            req.setAttribute("current", UserServiceImpl.getUserService().getCurrentUser());
             if (myPage instanceof TextPage) {
                 req.getRequestDispatcher("textPage.jsp").forward(req, resp);
             } else {
-
+                req.setAttribute("userAlreadySubmitted", false);
+                AssignmentPage assignmentPage = (AssignmentPage) myPage;
+                for (Assignment assignment : assignmentPage.getAssignments()) {
+                    if (assignment.getStudent().equals(UserServiceImpl.getUserService().getCurrentUser())) {
+                        req.setAttribute("userAlreadySubmitted", true);
+                    }
+                }
                 req.getRequestDispatcher("assignmentPage.jsp").forward(req, resp);
             }
-        } catch (PageNotFoundException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Override
