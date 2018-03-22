642f1404e1966435780897759b27bc51c6661112
Farago Bence <semmiertelme13@gmail.com>
2018-03-22 10:43:30 +0100

    Page servlet fixed.

--- src/main/java/com/codecool/lms/servlet/PageServlet.java
+++ src/main/java/com/codecool/lms/servlet/PageServlet.java
@@ -2,6 +2,7 @@ package com.codecool.lms.servlet;
 
 import com.codecool.lms.exception.PageNotFoundException;
 import com.codecool.lms.model.Page;
+import com.codecool.lms.model.TextPage;
 import com.codecool.lms.service.PageServiceImpl;
 
 import javax.servlet.ServletException;
@@ -16,18 +17,18 @@ public class PageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String type = req.getParameter("type");
         String title = req.getParameter("title");
         try {
             Page myPage = PageServiceImpl.getPageService().findPageByTitle(title);
             req.setAttribute("page", myPage);
+            if (myPage instanceof TextPage) {
+                req.getRequestDispatcher("textPage.jsp").forward(req, resp);
+            } else {
+
+                req.getRequestDispatcher("assignmentPage.jsp").forward(req, resp);
+            }
         } catch (PageNotFoundException e) {
             e.printStackTrace();
-        }
-        if (type.equals("text")) {
-            req.getRequestDispatcher("textPage.jsp").forward(req, resp);
-        } else {
-            req.getRequestDispatcher("assignmentPage.jsp").forward(req, resp);
         }
     }
 
@@ -35,8 +36,6 @@ public class PageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String title = req.getParameter("title");
         String published = req.getParameter("published");
-
-        try {
             if (published.equals("true")) {
                 PageServiceImpl.getPageService().findPageByTitle(title).depublish();
                 req.setAttribute("message", "Page is unpublished");
@@ -47,9 +46,6 @@ public class PageServlet extends HttpServlet {
             Page myPage = PageServiceImpl.getPageService().findPageByTitle(title);
 
             req.setAttribute("page", myPage);
-        } catch (PageNotFoundException e) {
-            e.printStackTrace();
-        }
         req.getRequestDispatcher("redirectHome.jsp").forward(req, resp);
     }
 }
