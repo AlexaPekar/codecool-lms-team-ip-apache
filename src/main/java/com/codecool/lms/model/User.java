3f7ebee50939010c8bd96a833e00e6b7990de568
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-03-22 11:39:57 +0100

    add setters to User class

--- src/main/java/com/codecool/lms/model/User.java
+++ src/main/java/com/codecool/lms/model/User.java
@@ -23,4 +23,12 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
