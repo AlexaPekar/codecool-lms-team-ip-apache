509c59a475fa50f534aa8a0d803070c831380b4a
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-04-03 10:40:47 +0200

    add a new condition to change the users role

--- src/main/java/com/codecool/lms/servlet/UsersServlet.java
+++ src/main/java/com/codecool/lms/servlet/UsersServlet.java
@@ -1,6 +1,10 @@
 package com.codecool.lms.servlet;
 
+import com.codecool.lms.exception.UserAlreadyRegisteredException;
+import com.codecool.lms.model.Mentor;
+import com.codecool.lms.model.Student;
 import com.codecool.lms.model.User;
+import com.codecool.lms.service.UserService;
 import com.codecool.lms.service.UserServiceImpl;
 
 import javax.servlet.ServletException;
@@ -26,18 +30,37 @@ public class UsersServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User currentUser = UserServiceImpl.getUserService().getCurrentUser();
+        if (req.getParameter("type").equals("Mentor")) {
+            UserServiceImpl.getUserService().deleteUser(currentUser.getName());
+            Mentor mentor = new Mentor(currentUser.getName(),currentUser.getEmail(),currentUser.getPassword());
+            try {
+                UserServiceImpl.getUserService().register(mentor);
+            } catch (UserAlreadyRegisteredException e) {
+                e.printStackTrace();
+            }
+        }else{
+            UserServiceImpl.getUserService().deleteUser(currentUser.getName());
+            Student student = new Student(currentUser.getName(), currentUser.getEmail(), currentUser.getPassword());
+            try {
+                UserServiceImpl.getUserService().register(student);
+            } catch (UserAlreadyRegisteredException e) {
+                e.printStackTrace();
+            }
+        }
         if (req.getParameter("newName").length() > 0) {
-            UserServiceImpl.getUserService().getCurrentUser().setName(req.getParameter("newName"));
+            currentUser.setName(req.getParameter("newName"));
         } else {
-            req.getParameter(UserServiceImpl.getUserService().getCurrentUser().getName());
+            req.getParameter(currentUser.getName());
         }
         if (req.getParameter("newPassword").length() >= 8 &&
                 req.getParameter("newPassword").equals(req.getParameter("secondPasswordToCheck"))) {
-            UserServiceImpl.getUserService().getCurrentUser().setPassword(req.getParameter("newPassword"));
+            currentUser.setPassword(req.getParameter("newPassword"));
             resp.sendRedirect("home");
         } else {
             req.setAttribute("message", "Invalid password. Try again.");
             req.getRequestDispatcher("redirectProfile.jsp").forward(req, resp);
         }
+
     }
 }
