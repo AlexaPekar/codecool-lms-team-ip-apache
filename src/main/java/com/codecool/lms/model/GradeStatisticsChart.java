05e61d474c2165f578104a89145582d572e7806c
HajnalFeri <kalacsf1@gmail.com>
2018-04-04 15:05:58 +0200

    grade chart class created

--- src/main/java/com/codecool/lms/model/GradeStatisticsChart.java
+++ src/main/java/com/codecool/lms/model/GradeStatisticsChart.java
@@ -1,21 +1,37 @@
 package com.codecool.lms.model;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.ApplicationFrame;
 
-public class GradeStatisticsChart extends ApplicationFrame{
+import java.awt.*;
 
-    public GradeStatisticsChart() {
-        super();
+public class GradeStatisticsChart {
 
-    }
+    public GradeStatisticsChart() {}
 
-    private static DefaultCategoryDataset createDataset() {
+    public DefaultCategoryDataset createDataset(int grade, int maxScore, String row, String column) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.setValue();
+        dataset.setValue(calculateGradePercentage(grade, maxScore), row, column);
+        return dataset;
     }
 
     public int calculateGradePercentage(int grade, int maxScore) {
         return (grade / maxScore) * 100;
+    }
+
+    public JFreeChart createChart(DefaultCategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Grade Statistics", // chart title
+            "Date", // domain axis label
+            "Grade percentage", dataset,
+            PlotOrientation.VERTICAL, // orientation
+            true, // include legend
+            true, // tooltips?
+            false // URLs?
+        );
+        chart.setBackgroundPaint(Color.white);
+        return chart;
     }
 }
