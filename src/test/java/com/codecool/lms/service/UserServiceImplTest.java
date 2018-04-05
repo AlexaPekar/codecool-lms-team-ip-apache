590fb9fe838082e757fdb32ce24ce2d5fbc797e1
Farago Bence <semmiertelme13@gmail.com>
2018-04-05 14:33:10 +0200

    Implemented UserServiceImpl unit tests

--- src/test/java/com/codecool/lms/service/UserServiceImplTest.java
+++ src/test/java/com/codecool/lms/service/UserServiceImplTest.java
@@ -3,9 +3,15 @@ package com.codecool.lms.service;
 import com.codecool.lms.exception.UserAlreadyRegisteredException;
 import com.codecool.lms.exception.UserNotFoundException;
 import com.codecool.lms.exception.WrongPasswordException;
+import com.codecool.lms.model.Day;
+import com.codecool.lms.model.Mentor;
+import com.codecool.lms.model.Student;
 import com.codecool.lms.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,15 +20,26 @@ class UserServiceImplTest {
     User user1;
     User user2;
     User user3;
+    Day day1;
+    Day day2;
 
     @BeforeEach
     void setUp() throws UserAlreadyRegisteredException {
         userServiceImpl = new UserServiceImpl();
-        user1 = new User("Dagobert", "kacsamesek@gmail.com", "cash");
-        user2 = new User("Gardener", "iamagardener@freemail.hu", "palm3");
-        user3 = new User("Conchita Wurst", "ihavebeard@homo.com", "celeb88");
+        user1 = userServiceImpl.createUser("kacsamesek@gmail.com", "Dagobert", "cash", "Student");
+        user2 = userServiceImpl.createUser("iamagardener@freemail.hu", "Gardener", "palm3", "Student");
+        user3 = userServiceImpl.createUser("ihavebeard@homo.com", "Conchita Wurst", "celeb88", "Student");
+        List<Student> studentList1 = new ArrayList<>();
+        List<Student> studentList2 = new ArrayList<>();
+        studentList1.add((Student) user1);
+        studentList2.add((Student) user2);
+        studentList2.add((Student) user1);
+        day1 = new Day(studentList1, "2017-09-13");
+        day2 = new Day(studentList1, "2017-09-14");
+        userServiceImpl.addDay(day1);
         userServiceImpl.register(user1);
         userServiceImpl.register(user2);
+
     }
 
     @Test
@@ -47,5 +64,53 @@ class UserServiceImplTest {
         assertThrows(WrongPasswordException.class, () -> {
             userServiceImpl.findUserByEmail("kacsamesek@gmail.com", "123");
         });
+    }
+
+
+    @Test
+    void createUser() {
+        assertTrue(userServiceImpl.createUser("semmiertelme13@gmail.com", "Bence Farago", "lalalalala23", "Student") instanceof Student);
+        assertTrue(userServiceImpl.createUser("semmiertelme13@gmail.com", "kancsal Jancsi", "jancsika23", "Mentor") instanceof Mentor);
+    }
+
+    @Test
+    void findUserByName() {
+        assertEquals(user1, userServiceImpl.findUserByName("Dagobert"));
+        assertEquals("palm3", userServiceImpl.findUserByName("Gardener").getPassword());
+    }
+
+    @Test
+    void addDay() {
+        assertEquals(1, userServiceImpl.getDays().size());
+        userServiceImpl.addDay(day2);
+        assertEquals(2, userServiceImpl.getDays().size());
+
+    }
+
+    @Test
+    void dayExist() {
+        assertTrue(userServiceImpl.dayExist("2017-09-13"));
+        assertFalse(userServiceImpl.dayExist("2017-09-22"));
+    }
+
+    @Test
+    void findDayByDate() {
+        assertEquals(day1, userServiceImpl.findDayByDate("2017-09-13"));
+    }
+
+    @Test
+    void deleteUser() {
+        assertEquals(2, userServiceImpl.getUsers().size());
+        userServiceImpl.deleteUser("Dagobert");
+        assertEquals(1, userServiceImpl.getUsers().size());
+    }
+
+    @Test
+    void createAttendStudentList() {
+        String[] strings = new String[1];
+        strings[0] = "Dagobert";
+        List<Student> students = userServiceImpl.createAttendStudentList(strings);
+        assertEquals(1, students.size());
+
     }
 }
