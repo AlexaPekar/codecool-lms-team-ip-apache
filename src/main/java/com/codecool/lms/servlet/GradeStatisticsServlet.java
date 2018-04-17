721c20a0329c083a46ca9ff737cec898c3dd2e85
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-04-17 18:19:37 +0200

    add modified doGet methods

--- src/main/java/com/codecool/lms/servlet/GradeStatisticsServlet.java
+++ src/main/java/com/codecool/lms/servlet/GradeStatisticsServlet.java
@@ -1,22 +1,25 @@
 package com.codecool.lms.servlet;
 
+import com.codecool.lms.dao.DatabasePagesDao;
+import com.codecool.lms.exception.UserNotFoundException;
 import com.codecool.lms.model.Assignment;
 import com.codecool.lms.model.GradeStatisticsChart;
 import com.codecool.lms.model.Student;
 import com.codecool.lms.model.User;
-import com.codecool.lms.service.PageServiceImpl;
+import com.codecool.lms.service.PageServiceDaoImpl;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.Base64;
 import java.util.List;
@@ -26,28 +29,41 @@ public class GradeStatisticsServlet extends AbstractServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User currentUser = (User) req.getSession().getAttribute("currentUser");
-        req.setAttribute("current", currentUser);
-        List<Assignment> userAssignments = PageServiceImpl.getPageService().currentUserAssingments(currentUser);
-
-        req.setAttribute("userAssignments", userAssignments);
-        double max = PageServiceImpl.getPageService().findEvaluatedPercent((Student) req.getSession().getAttribute("currentUser"));
-        DecimalFormat df2 = new DecimalFormat(".##");
-        req.setAttribute("max", df2.format(max));
+        try (Connection connection = getConnection(req.getServletContext())) {
+            DatabasePagesDao databasePagesDao = new DatabasePagesDao(connection);
+            PageServiceDaoImpl pageServiceDao = new PageServiceDaoImpl(databasePagesDao);
 
 
-        //Chart
-        GradeStatisticsChart gradeStatisticsChart = new GradeStatisticsChart();
-        DefaultCategoryDataset dataset = gradeStatisticsChart.createDataset(currentUser);
-        JFreeChart chart = gradeStatisticsChart.createChart(dataset);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        OutputStream b64os = Base64.getEncoder().wrap(baos);
-        ChartUtils.writeChartAsPNG(b64os, chart, 780, 500);
-        String b64 = new String(baos.toByteArray());
-
-        req.setAttribute("b64", b64);
+            User currentUser = (User) req.getSession().getAttribute("currentUser");
+            req.setAttribute("current", currentUser);
+            List<Assignment> userAssignments = pageServiceDao.currentUserAssingments(currentUser);
 
 
-        req.getRequestDispatcher("gradeStatistics.jsp").forward(req, resp);
+            req.setAttribute("userAssignments", userAssignments);
+            double max = pageServiceDao.findEvaluatedPercent((Student) currentUser);
+            DecimalFormat df2 = new DecimalFormat(".##");
+            req.setAttribute("max", df2.format(max));
+
+
+            //Chart
+            GradeStatisticsChart gradeStatisticsChart = new GradeStatisticsChart();
+            DefaultCategoryDataset dataset = gradeStatisticsChart.createDataset(currentUser);
+            JFreeChart chart = gradeStatisticsChart.createChart(dataset);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            OutputStream b64os = Base64.getEncoder().wrap(baos);
+            ChartUtils.writeChartAsPNG(b64os, chart, 780, 500);
+            String b64 = new String(baos.toByteArray());
+
+            req.setAttribute("b64", b64);
+
+
+            req.getRequestDispatcher("gradeStatistics.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            req.setAttribute("message", e.getMessage());
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        } catch (UserNotFoundException e) {
+            req.setAttribute("message", "User not found!");
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        }
     }
 }
--- src/main/java/com/codecool/lms/servlet/GradingServlet.java
+++ src/main/java/com/codecool/lms/servlet/GradingServlet.java
@@ -1,16 +1,19 @@
 package com.codecool.lms.servlet;
 
 
+import com.codecool.lms.dao.DatabasePagesDao;
+import com.codecool.lms.exception.UserNotFoundException;
 import com.codecool.lms.model.Assignment;
-import com.codecool.lms.service.PageServiceImpl;
+import com.codecool.lms.service.PageServiceDaoImpl;
 import com.codecool.lms.service.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet("/grading")
@@ -18,10 +21,21 @@ public class GradingServlet extends AbstractServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Assignment> assignments = PageServiceImpl.getPageService().getAssignments();
-        req.setAttribute("current", req.getSession().getAttribute("currentUser"));
-        req.setAttribute("assignments", assignments);
-        req.getRequestDispatcher("grading.jsp").forward(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())) {
+            DatabasePagesDao databasePagesDao = new DatabasePagesDao(connection);
+            PageServiceDaoImpl pageServiceDao = new PageServiceDaoImpl(databasePagesDao);
+
+            List<Assignment> assignments = pageServiceDao.getAssignments();
+            req.setAttribute("current", req.getSession().getAttribute("currentUser"));
+            req.setAttribute("assignments", assignments);
+            req.getRequestDispatcher("grading.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            req.setAttribute("message", e.getMessage());
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        } catch (UserNotFoundException e) {
+            req.setAttribute("message", "User not found!");
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        }
     }
 
     @Override
