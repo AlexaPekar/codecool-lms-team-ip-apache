e2d1f9e361547fe4ef43aa61034295168e2c5b18
Farago Bence <semmiertelme13@gmail.com>
2018-04-17 09:13:16 +0200

    Implemented the dao methods to the servlets

--- src/main/java/com/codecool/lms/servlet/DeletePageServlet.java
+++ src/main/java/com/codecool/lms/servlet/DeletePageServlet.java
@@ -1,20 +1,29 @@
 package com.codecool.lms.servlet;
 
-import com.codecool.lms.service.PageServiceImpl;
+import com.codecool.lms.dao.DatabasePagesDao;
+import com.codecool.lms.service.PageServiceDaoImpl;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 
 @WebServlet("/delete")
 public class DeletePageServlet extends AbstractServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String pageName = req.getParameter("page");
-        PageServiceImpl.getPageService().removePage(pageName);
-        resp.sendRedirect("home");
+        try (Connection connection = getConnection(req.getServletContext())) {
+            DatabasePagesDao databasePagesDao = new DatabasePagesDao(connection);
+            PageServiceDaoImpl pageServiceDao = new PageServiceDaoImpl(databasePagesDao);
+            String pageName = req.getParameter("page");
+            pageServiceDao.removePage(pageName);
+        } catch (SQLException e) {
+            req.setAttribute("error", e.getMessage());
+        } finally {
+            resp.sendRedirect("home");
+        }
     }
 }
